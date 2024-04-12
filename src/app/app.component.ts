e13a545c8efe1927306a882a986b121717f304ec
ivanics.adam <ivanics.adam@gbsolutions.io>
2024-04-12 19:44:34 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,13 @@ import { FooterComponent } from 'src/app/footer/footer.component';
 import { HeaderComponent } from 'src/app/header/header.component';
 import { LocationComponent } from "src/app/location/location.component";
 import { StoryComponent } from "src/app/story/story.component";
-import { TimelineComponent } from "src/app/timeline/timeline.component";
-import { GiftComponent } from "src/app/gift/gift.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet ,FooterComponent, HeaderComponent, LocationComponent, StoryComponent, TimelineComponent ,GiftComponent]
+  imports: [RouterOutlet ,FooterComponent, HeaderComponent, LocationComponent, StoryComponent]
 })
 export class AppComponent  {
   title = 'ng-wedding-site';
